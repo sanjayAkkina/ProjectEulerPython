e4628de8157b6081fa3520b96c840c8a0e6c4ac7
Sanjay Akkina <sakkina1@uncc.edu>
2017-02-25 05:53:04 +0000

    Solved Problem 16

--- Problem16.py
+++ Problem16.py
@@ -1,14 +1,20 @@
+"""
+PROBLEM 16
+What is the sum of the digits of the number 2^1000?
+
+Modified to take user's response for the base
+and the exponent for an exponential function
+"""
+
 base = input("Input Base: ")
 exponent = input("Input Exponent: ")
 
 exponentFunction = base**exponent
-tempNum = str(exponentFunction)
+tempNum = str(exponentFunction) #Converts integer into a string
 digitsArray = []
 
-for digits in tempNum:
+for digits in tempNum: #Stores digits into an array and converts into integers
     digitsArray.append(int(digits))
-
-print digitsArray
 
 sum = 0
 
@@ -16,21 +22,3 @@ for i in digitsArray:
     sum += i
 
 print "Sum of Digits: ", sum
-    
-
-"""
-num = 2**15
-tempNum = str(num)
-array = []
-
-for digit in tempNum:
-    array.append(int(digit))
-    
-print array
-
-sum = 0
-for i in array:
-    sum += i
-    
-print sum
-"""
