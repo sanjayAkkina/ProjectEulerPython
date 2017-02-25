9c0013869770774c8c196df51ed6d82e06c82716
Sanjay Akkina <sakkina1@uncc.edu>
2017-02-25 05:49:44 +0000

    Takes users response and displays all the digits in the exponential function

--- Problem16.py
+++ Problem16.py
@@ -1,3 +1,24 @@
+base = input("Input Base: ")
+exponent = input("Input Exponent: ")
+
+exponentFunction = base**exponent
+tempNum = str(exponentFunction)
+digitsArray = []
+
+for digits in tempNum:
+    digitsArray.append(int(digits))
+
+print digitsArray
+
+sum = 0
+
+for i in digitsArray:
+    sum += i
+
+print "Sum of Digits: ", sum
+    
+
+"""
 num = 2**15
 tempNum = str(num)
 array = []
@@ -12,3 +33,4 @@ for i in array:
     sum += i
     
 print sum
+"""
